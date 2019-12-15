9f55dbd9522a79be1aabc4e789acf89bdf0cc9c3
Leonid <chileonid@gmail.com>
2019-12-15 22:01:57 +1100

    Removed duplicate main.go

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt" 
-import "github.com/bwmarrin/discordgo"
-
-func main() {
-	discord, err := discordgo.New("Bot " + "authToken")
-	
-}
--- src/main.go
+++ src/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import "fmt" 
+import "github.com/bwmarrin/discordgo"
 
 func main() {
+	discord, err := discordgo.New("Bot " + "authToken")
 	
 }
